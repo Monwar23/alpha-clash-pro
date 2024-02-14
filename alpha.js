b11ca18b5d07640d3630dc46a95e848dfe19181d
monwar23 <monwar2345@gmail.com>
2024-02-15 00:07:09 +0600

    score life

--- alpha.js
+++ alpha.js
@@ -7,6 +7,41 @@
 //     const playgroundSection=document.getElementById('play-ground')
 //     playgroundSection.classList.remove('hidden')
 // }
+function handleKeyboardButtonPress(event){
+    const playerPress=event.key
+    console.log(playerPress)
+    // expected press
+    const currentAlphabetElement=document.getElementById('current-alphabet')
+    const currentAlphabet=currentAlphabetElement.innerText
+    const expectedAlphabet=currentAlphabet.toLowerCase()
+    // check
+    if(playerPress===expectedAlphabet){
+        console.log('yes')
+        //  score update    
+        //  1.get correct score
+        const currentScoreElement=document.getElementById('current-score')
+        const currentScoreText=currentScoreElement.innerText
+        const currentScore=parseInt(currentScoreText)
+        // 2.increase score 1
+        const NewScore=currentScore+1
+        // 3.show score
+        currentScoreElement.innerText=NewScore
+        // start a new round
+        removeBackgroundColorById(expectedAlphabet)
+        continueGame()
+    }
+    else{
+        console.log('no')
+        const currentLifeElement=document.getElementById('current-life')
+        const currentLifeText=currentLifeElement.innerText
+        const currentLife=parseInt(currentLifeText)
+        const newLife=currentLife-1
+        currentLifeElement.innerText=newLife
+
+    }
+}
+document.addEventListener('keyup',handleKeyboardButtonPress)
+
 function continueGame(){
     //1. generate a random alphabet
     const alphabet=getARandomAlphabet(); 
