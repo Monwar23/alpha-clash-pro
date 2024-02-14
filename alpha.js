9adcbd1dc6dce4e754374664529de68075e68a45
monwar23 <monwar2345@gmail.com>
2024-02-14 22:38:55 +0600

    first part finished

--- alpha.js
+++ alpha.js
@@ -1,3 +1,25 @@
+// function play(){
+//     // hide the home screen
+//     const homeSection=document.getElementById('home-screen')
+//     homeSection.classList.add('hidden')
+
+//     // show the playground
+//     const playgroundSection=document.getElementById('play-ground')
+//     playgroundSection.classList.remove('hidden')
+// }
+function continueGame(){
+    //1. generate a random alphabet
+    const alphabet=getARandomAlphabet(); 
+    // 2.show this
+    const currentAlphabetElement=document.getElementById('current-alphabet')
+    currentAlphabetElement.innerText=alphabet 
+    // set bg color
+    setBackgroundColorById(alphabet)
+}
+
+
 function play(){
-    console.log('yoyo')
+    hideElementByID('home-screen');
+    showElementByID('play-ground');
+    continueGame()
 }
